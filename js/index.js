fc443913194861d22b64b46e393bdad33f7cc0c9
Boyoung Kwon <boyoung.kwon@gmail.com>
2016-02-05 18:19:27 -0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by boyoung on 2/5/16.
+ */
+var allQuestions = [{question: "When you drive through a construction zone, you should:",
+                    choices: ["Slow down to watch the worker.",
+                              "Decrease your following distance.",
+                              "Pass the construction zone carefully and not 'rubberneck'."],
+                    correctAnswer:2},
+                     {question: "To make a right turn at the corner, you:",
+                       choices: ["May not enter the bicycle lane.",
+                                 "Should only merge into the bicycle lane if you stop before turning.",
+                                 "Must merge into the bicycle lane before turning."],
+                       correctAnswer:2
+                     }];
+
+
+
+
+
