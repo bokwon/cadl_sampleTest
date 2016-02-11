d1a837e64a2dffa89a441123484452568eb9e97e
Boyoung Kwon <boyoung.kwon@gmail.com>
2016-02-11 14:18:45 -0800

    implement to show score, prev click event

--- js/index.js
+++ js/index.js
@@ -2,22 +2,35 @@
  * Created by boyoung on 2/5/16.
  */
 
-//TODO
-// add at least 8 more questions
-var allQuestions = [{question: "When you drive through a construction zone, you should:",
-                    choices: ["Slow down to watch the worker.",
-                              "Decrease your following distance.",
-                              "Pass the construction zone carefully and not 'rubberneck'."],
-                    correctAnswer:2},
-                     {question: "To make a right turn at the corner, you:",
-                       choices: ["May not enter the bicycle lane.",
-                                 "Should only merge into the bicycle lane if you stop before turning.",
-                                 "Must merge into the bicycle lane before turning."],
-                       correctAnswer:2
-                     }];
+var allQuestions = [
+  {question: "When you drive through a construction zone, you should:",
+  choices: ["Slow down to watch the worker.", "Decrease your following distance.", "Pass the construction zone carefully and not 'rubberneck'."],
+  correctAnswer:2},
+  {question: "To make a right turn at the corner, you:",
+  choices: ["May not enter the bicycle lane.", "Should only merge into the bicycle lane if you stop before turning.", "Must merge into the bicycle lane before turning."],
+  correctAnswer:2},
+  {question: "If a traffic signal light is now working, you must:",
+  choices: ["Stop, then proceed when safe", "Stop before entering the intersection and let all other traffic go first.", "Slow down or stop, only if necessary."],
+  correctAnswer:0},
+  {question: "A pedestrian is crossing your lane but there is no marked crosswalk. You should",
+  choices: ["Make sure the pedestrian sees you, but continue driving", "Carefully drive around the pedestrian", "Stop and let the pedestrian cross the street."],
+  correctAnswer:2},
+  {question: "Always use your seat belt:",
+  choices: ["Unless the vehicle was built before 1978", "Unless you are in a limousine", "When the vehicle is equipped with seat belts"],
+  answer:2},
+  {question: "The extra space in front of a large truck is needed for",
+  choices: ["Other drivers when merging onto a freeway", "The truck driver to stop the vehicle.", "Other drivers when they want to slow down"],
+  answer:1},
+  {question: "Roads are slippery after it first starts to rain. When the road is slippery you should:",
+  choices: ["Avoid making fast turns and fast stops", "Test your tires traction while going uphill", "Decrease the distance you look ahead of your vehicle."],
+  answer:0},
+  {question: "Collisions can happen more often when",
+  choices: ["All vehicles are traveling about the same speed.", "One lane of traffic is traveling faster than the other lane.", "One vehicle is traveling faster or slower than the flow of the traffic."],
+  answer:2}
+];
 
 var questionCounter = 0;
-var choices = [];
+var selections = [];
 var quiz = $("#quiz");
 
 var prev = $("#prev");
@@ -45,7 +58,7 @@ function createRadios(index){
 
   for(var i=0; i<allQuestions[index].choices.length; i++){
     item = $('<li>');
-    input = '<input type="radio" name="answer" value=' + i + '/>';
+    input = '<input type="radio" name="answer" value=' + i + ' />';
     input += allQuestions[index].choices[i];
     item.append(input);
     choices.append(item);
@@ -60,6 +73,9 @@ function displayNext(){
     if(questionCounter < allQuestions.length) {
       var nextQuestion = appendQuestion(questionCounter);
       quiz.append(nextQuestion).fadeIn();
+      if(selections[questionCounter] !== undefined){
+        $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
+      }
 
       if (questionCounter === 1) {
         prev.show();
@@ -77,28 +93,30 @@ function displayNext(){
       start.show();
       start.css('display', 'inline-block');
     }
-
   });
 }
 
-//TODO
-//Implement showing score.
 function displayScore(){
   var score = $('<p>').addClass('qContainer');
-  score.append('done');
+  var correct = 0;
+  for(var i=0; i < selections.length; i++){
+    if(selections[i] === allQuestions[i].correctAnswer){
+      correct++;
+    }
+  }
 
+  score.append('You have answered ' + correct + ' questions correctly out of ' + selections.length + ' questions!');
   return score;
 }
 
 function choose(){
-  choices[questionCounter] = +$('input[name="answer"]:checked').val();
+  selections[questionCounter] = +$('input[name="answer"]:checked').val();
 }
 
 $(document).ready(function(){
 
   // show first question
   displayNext();
-  choose();
 
   next.on("click", function(e){
     e.preventDefault();
@@ -106,19 +124,22 @@ $(document).ready(function(){
     if ($('input[name="answer"]:checked').length === 0) {
       alert("Please select the answer!");
     } else {
+      choose();
       questionCounter++;
       displayNext();
     }
   });
 
-  //TODO
-  //when prev button is clicked, should go back to previous question.
+  prev.on("click", function(){
+    questionCounter--;
+    displayNext();
+  });
 
   start.on("click", function(e){
     e.preventDefault();
 
     questionCounter = 0;
-    choices = [];
+    selections = [];
     displayNext();
     start.hide();
   })
